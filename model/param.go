166cc073e4c41cc761e36e2845cb63c1f277548d
Zheng， XuebingX <xuebingx.zheng@intel.com>
2018-09-20 14:58:24 +0800

    Add AliPayParam interface

--- model/param.go
+++ model/param.go
@@ -1,5 +1,17 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type AliPayParam interface {
+	APIName() string
+	Params() map[string]string
+	ExtJSONParamName() string
+	ExtJSONParamValue() string
+}
+
 //AliPayTradeWapPay https://docs.open.alipay.com/203/107090
 type AliPayTradeWapPay struct {
 	NotifyURL string `json:"-"`
@@ -21,4 +33,36 @@ type AliPayTradeWapPay struct {
 	EnablePayChannels  string `json:"enable_pay_channels,omitempty"`
 	DisablePayChannels string `json:"disable_pay_channels,omitempty"`
 	StoreId            string `json:"store_id,omitempty"`
+}
+
+func (this AliPayTradeWapPay) APIName() string {
+	return "alipay.trade.wap.pay"
+}
+
+func (this AliPayTradeWapPay) Params() map[string]string {
+	var m = make(map[string]string)
+	m["notify_url"] = this.NotifyURL
+	return m
+}
+
+func (this AliPayTradeWapPay) ExtJSONParamName() string {
+	return "biz_content"
+}
+
+func (this AliPayTradeWapPay) ExtJSONParamValue() string {
+	bytes, err := json.Marshal(this)
+	if err != nil {
+		fmt.Errorf("JSON marshal err", err)
+		return ""
+	}
+	return string(bytes)
+}
+
+func marshal(obj interface{}) string {
+	bytes, err := json.Marshal(obj)
+	if err != nil {
+		return ""
+	}
+	return string(bytes)
+
 }
